7305f14d2ae910fa0ae8369a854a423369119c1c
Anish Das <139654946+anish2409@users.noreply.github.com>
2024-07-15 00:20:13 +0530

    Add files via upload

---
+++ 2_operators.js
@@ -0,0 +1,54 @@
+//Arithmatic Operator
+
+const x = 5;
+const y = 3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+
+//Assignment Operator
+
+let a=5;
+a+= 4;
+console.log(a);
+
+let b=6;
+b-= 2;
+console.log(b);
+
+// Comparison Operator
+
+let p = 5;
+let q = 5;
+
+z=(p>q && p<q);
+console.log(z);
+z=(p>=q && p<=q);
+console.log(z);
+z=(p==q && p===q);
+console.log(z);
+
+// Logical Operator
+let s= 6;
+let t = 4;
+
+o=(s>t && s<t);
+console.log(o);
+o=(s>t || s<t);
+console.log(o);
+o=!s;
+console.log(o);
+
+//Ternary Operator
+let num =10;
+let result = (num>=0)?"Positive" : "Negetive";
+console.log(result);
+
+
+
+
+
+
