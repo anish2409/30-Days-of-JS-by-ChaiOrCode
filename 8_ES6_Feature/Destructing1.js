dd91d38be2382f0fc1e24c3bbc756c0ef8f152f6
Anish Das <139654946+anish2409@users.noreply.github.com>
2024-07-22 19:10:24 +0530

    Add files via upload

---
+++ 8_ES6_Feature/Destructing1.js
@@ -0,0 +1,11 @@
+// assigning object attributes to variables
+const book= {
+    title: 'Atomic Habit',
+    writer: 'anish'   
+}
+
+// destructuring assignment
+let {title,writer } = book;
+
+console.log(title); // Atomic habit
+console.log(writer); // anish
