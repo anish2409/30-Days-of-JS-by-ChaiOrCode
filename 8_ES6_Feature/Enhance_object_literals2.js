f34e64d2155a88fb7efcddac38bdac6f34c3b48c
Anish Das <139654946+anish2409@users.noreply.github.com>
2024-07-22 19:11:35 +0530

    Add files via upload

---
+++ 8_ES6_Feature/Enhance_object_literals2.js
@@ -0,0 +1,14 @@
+// Variables to use as property names
+const propName1 = "firstName";
+const propName2 = "lastName";
+const propName3 = "age";
+
+// Create an object with computed property names
+const person = {
+    [propName1]: "Anish",
+    [propName2]: "Das",
+    [propName3]: 21
+};
+
+// Log the object to the console
+console.log(person);
