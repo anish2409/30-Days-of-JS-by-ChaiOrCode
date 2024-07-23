0f31bba3b2d95e51a943e37dec598f0de8494a91
Anish Das <139654946+anish2409@users.noreply.github.com>
2024-07-23 10:39:21 +0530

    Add files via upload

---
+++ 8_ES6_Feature/rest_operator.js
@@ -0,0 +1,6 @@
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+
+console.log("Sum:", sum(1, 2, 3, 4, 5)); // Sum: 15
+console.log("Sum:", sum(10, 20)); // Sum: 30
